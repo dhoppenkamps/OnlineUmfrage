6a486c7c7baa67062cb76f6f0c873ea2c5a91075
Dominik Hoppenkamps <dominik.hoppenkamps@web.de>
2014-05-14 21:17:14 +0200

    Grobentwurf UML-Klassendiagramm

---
+++ src/main/java/de/bkah/kundenumfrage/model/AuswertungBean.java
@@ -0,0 +1,26 @@
+package de.bkah.kundenumfrage.model;
+
+public class AuswertungBean {
+	
+	private Umfrage umfrage;
+	private Antwort antwort;
+	
+	
+	public Umfrage getUmfrage() {
+		return umfrage;
+	}
+	public void setUmfrage(Umfrage umfrage) {
+		this.umfrage = umfrage;
+	}
+	public Antwort getAntwort() {
+		return antwort;
+	}
+	public void setAntwort(Antwort antwort) {
+		this.antwort = antwort;
+	}
+	
+	public void werteAus() {
+		
+	}
+
+}
